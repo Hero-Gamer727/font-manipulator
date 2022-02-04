9942b55bf9cfd354baa492632f277b5dcf4b5481
Hero-Gamer727 <83517823+Hero-Gamer727@users.noreply.github.com>
2022-02-04 14:56:08 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+nose_x=0;
+nose_y=0;
+left_wrist_x=0;
+right_wrist_x=0;
+gap_of_wrist=0;
+
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -18,7 +25,23 @@ function modelLoaded() {
 function gotResults(results){
 if(results.length > 0){
     console.log(results);
-
+    nose_x=results[0].pose.nose.x;
+    nose_y=results[0].pose.nose.y;
+    
+    left_wrist_x=results[0].pose.leftWrist.x;
+    right_wrist_x=results[0].pose.rightWrist.x;
+    console.log( "leftWrist_x = "+left_wrist_x);
+    console.log( "rightWrist_x = "+right_wrist_x);
+    gap_of_wrist= floor(left_wrist_x-right_wrist_x);
 }
 }
 
+function draw(){
+    document.getElementById("font-size_number").innerHTML=gap_of_wrist+" px ";
+
+    background("grey");
+    textSize(gap_of_wrist);
+    fill("red");
+    text("Aariz", nose_x,nose_y);
+    
+}
